0009edb933d771157e24b6ed7e8e2ff73b0f1983
sakamototaisei <taisei0403089@gmail.com>
2021-06-17 01:15:30 +0000

    genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,29 @@
 class Admin::GenresController < ApplicationController
+  
+  
   def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+  
+  def create
+    @genre = Genre.new(params_genre)
+    @genres = Genre.all
+    @genre.save
+    redirect_to admin_genres_path
   end
 
   def edit
   end
+  
+  def update
+  end
+  
+  
+  private
+  
+  def params_genre
+    params.require(:genre).permit(:name)
+  end
+  
 end
