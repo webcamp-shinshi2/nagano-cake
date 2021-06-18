4840a9b7f7f0f201ca600fb3e8642d68d7f02658
sakamototaisei <taisei0403089@gmail.com>
2021-06-18 08:47:42 +0000

    add footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,22 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  
+  def after_sign_in_path_for(resource)
+    if customer_signed_in?
+      customer_path(resource)
+    else
+      admin_path
+    end
+  end
+    
+  def after_sign_out_path_for(resource)
+    if customer_signed_out?
+      root_path
+    else
+      new_admin_session
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
