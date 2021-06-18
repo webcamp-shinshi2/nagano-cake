ca8c0cd5b0150307e1a31d701ff2b39bd53e87c8
Naru-hub <1208ny0324@gmail.com>
2021-06-18 03:09:27 +0000

    order newpage

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,6 +1,7 @@
 class Customers::OrdersController < ApplicationController
   def new
     @order = Order.new
+    
   end
 
   def confirm
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,6 @@
 class CartItem < ApplicationRecord
   belongs_to :product
   belongs_to :customer
+
+  validates :customer_id, :product_id, :amount, presence:true
 end
