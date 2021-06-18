ce7c87e0f4f709d6ebe90154a3f7d8925b2d27c0
EC2 Default User <ec2-user@ip-172-31-22-254.ec2.internal>
2021-06-18 07:51:08 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Customer.create!(last_name: "紳士",
+                first_name: "太郎",
+                last_name_kana: "シンシ",
+                first_name_kana: "タロウ",
+                email: "test@test.com",
+                password: "passward",
+                password_confirmation: "passward",
+                is_deleted: false,
+                postal_code: "1234567",
+                address: "東京都渋谷区代々木神園町0-0",
+                phone_number: "09012345678",
+                reset_password_token: "50",
+                )
