1ca01d734e731f43d5f1fca1c93f394119ef0b8c
Naru-hub <1208ny0324@gmail.com>
2021-06-18 07:58:58 +0000

    add product_controller

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -3,5 +3,9 @@ class Customers::ProductsController < ApplicationController
   end
 
   def show
+     # 商品情報の取得
+    @product = Product.find(params[:id])
+    # form_withのための空のインスタンス
+    @cart_items = CartItem.new
   end
 end
