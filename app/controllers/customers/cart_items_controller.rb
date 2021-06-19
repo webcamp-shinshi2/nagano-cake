6bf916dce1b9a981cf7a0c1087c660f9d9592ab7
Naru-hub <1208ny0324@gmail.com>
2021-06-19 12:04:08 +0000

    add all_destroy

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -14,12 +14,19 @@ class Customers::CartItemsController < ApplicationController
     cart_item = current_customer.cart_items.new(cart_item_params)
     cart_item.save
 
-
+    in_cart_item = CartItem.find_by(product_id: cart_item.product_id, customer_id: current_customer.id)
+    if in_cart_item.present?
+      in_cart_item.amount += cart_item.amount
+      in_cart_item.save
+    else
+      cart_item.save
+    end
+    redirect_to cart_items_path, notice: "カートに追加しました"
 
      # cart_item.product = params[:cart_item][:product_id]
     # cart_item.product = Cartitem.find_by(product_id: cart_item.product_id, customer_id: current_customer.id)
     # cart_item.product = CartItem.find_by(product_id: product_id, id: cart_item.id)
-    redirect_to cart_items_path
+
   end
 
   def update
@@ -29,9 +36,17 @@ class Customers::CartItemsController < ApplicationController
   end
 
   def destroy
+    cart_item = CartItem.find(params[:id])
+    # if cart_item.customer_id == current_customer.id
+      cart_item.destroy
+    # end
+    redirect_to cart_items_path, alert: "カートから商品を削除しました"
   end
 
-  def all_destory
+  def destroy_all
+    cart_items = current_customer.cart_items
+    cart_items.destroy_all
+    redirect_to cart_items_path, alert: "カートを空にしました"
   end
 
   private
