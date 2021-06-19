c18e931e4acb9f7f5c79d14f88f52836d52bb45f
sakamototaisei <taisei0403089@gmail.com>
2021-06-19 06:43:13 +0000

    start customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,33 @@
 class Admin::CustomersController < ApplicationController
+  
+  # before_action :authenticate_admin!
+  
   def index
+    @customers = Customer.all.pege(params[:pege]).per(10)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path
+    else
+      render :edit
+    end
+  end
+  
+  
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :phone_number, :email)
   end
 end
--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,6 +1,6 @@
 class Admin::GenresController < ApplicationController
   
-  # before_action :authenticate_admin!
+  before_action :authenticate_admin!
   
   def index
     @genres = Genre.all
