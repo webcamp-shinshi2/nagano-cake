1490c21b824c38f4d5c01d90ef913dcf048f8399
Naru-hub <1208ny0324@gmail.com>
2021-06-20 02:08:27 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,6 @@ Customer.create!(last_name: "紳士",
                 postal_code: "1234567",
                 address: "東京都渋谷区代々木神園町0-0",
                 phone_number: "09012345678",
-                reset_password_token: "50",
                 )
 
 Admin.create!(email: "admin@test.com",
@@ -34,15 +33,44 @@ Product.create!(name: "チーズケーキ",
                 is_sale: true,
                 )
 
- Product.create!(name: "ショートケーキ",
+Product.create!(name: "ショートケーキ",
                 genre_id: 1,
                 introduction: "ショートケーキ",
                 price: 500,
                 is_sale: true,
                 )
 
-  Address.create!(name: "淑女花子",
+Address.create!(name: "淑女花子",
                   customer_id: 1,
                   address: "埼玉県三郷市〇〇町0-0",
                   postal_code: "3456789"
                   )
+
+49.times do |n|
+    gimei = Gimei.unique.name
+    address = Gimei.unique.address
+
+    last_name = gimei.last.kanji
+    first_name = gimei.first.kanji
+    last_name_kana = gimei.last.katakana
+    first_name_kana = gimei.first.katakana
+
+    email = "example-#{n+1}@example.com"
+    password = "password"
+    postal_code = rand(1000000..9999999).to_s
+    phone_number = rand(1000000000..9999999999).to_s
+
+    Customer.create!(
+                    last_name:  last_name,
+                    first_name: first_name,
+                    last_name_kana: last_name_kana,
+                    first_name_kana: first_name_kana,
+                    email: email,
+                    password: password,
+                    password_confirmation: password,
+                    is_deleted: false,
+                    postal_code: postal_code,
+                    address: address,
+                    phone_number: phone_number
+                    )
+      end
