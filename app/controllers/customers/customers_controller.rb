a0eac96485abca09a2aa0530a8c7641869859a0a
EC2 Default User <ec2-user@ip-172-31-22-254.ec2.internal>
2021-06-20 01:42:49 +0000

    mypage start

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -1,10 +1,22 @@
 class Customers::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def confirm
+    @customer = current_customer
   end
+  
+  def update
+    
+  end
+  
+  def out
+    
+  end
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,8 @@ class Product < ApplicationRecord
     has_many :cart_items, dependent: :destroy
     has_many :order_details, dependent: :destroy
     
+    
+    
     def price
       (self.price * 1.10).round
     end
