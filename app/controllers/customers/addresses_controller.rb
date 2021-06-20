984f81888b3f883ad7078b495df5bd812cfe3485
sakamototaisei <taisei0403089@gmail.com>
2021-06-20 01:47:35 +0000

    strat address

--- app/controllers/customers/addresses_controller.rb
+++ app/controllers/customers/addresses_controller.rb
@@ -1,7 +1,25 @@
 class Customers::AddressesController < ApplicationController
+  
   def index
+  end
+  
+  def create
   end
 
   def edit
   end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  
+  private
+  
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+    
 end
