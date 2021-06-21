804d114b65dd590bd9b67287c32560c07a6d474d
sakamototaisei <taisei0403089@gmail.com>
2021-06-21 08:22:59 +0000

    address validates

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,5 +5,8 @@ class Address < ApplicationRecord
   # validates :postal_code, presence: true,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "半角数字のみで入力してください"}
 
   validates :address, :name, :postal_code, presence: true
-  validates :postal_code, length: {is: 7}, numericality: { only_integer: true}
+  
+  VALID_POSTAL_CODE_REGEX = /\A\d{3}[-]?\d{4}\z/ #正規表現を//で/正規表現/とサンドすることを忘れないよう
+  validates :postal_code, presence: true, format: { with: VALID_POSTAL_CODE_REGEX }
+
 end
