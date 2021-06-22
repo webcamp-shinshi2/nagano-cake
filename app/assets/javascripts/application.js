fa9d35bdb568576f21728fb809418c8572609297
sakamototaisei <taisei0403089@gmail.com>
2021-06-22 12:06:19 +0000

    add about text

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -53,3 +53,28 @@ $(document).on('turbolinks:load', function () {
     $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
   });
 });
+
+
+// about文字
+$(document).on('turbolinks:load', function () {
+  $('#hoge').textillate({
+    loop: true,  
+    minDisplayTime: 3000,  
+    initialDelay: 2000,  
+    autoStart: true,  
+    in: {
+      effect: 'fadeIn',  
+      delayScale: 1.5,  
+      delay: 20,  
+      sync: false,  
+      shuffle: false
+      },  
+    out: {
+      effect: 'fadeOut',  
+      delayScale: 1.5,  
+      delay: 50,  
+      sync: false,  
+      shuffle: false
+      }
+  });
+})
