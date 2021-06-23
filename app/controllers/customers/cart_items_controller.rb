1d436c733c6fe5305def38a92b76874591215060
riri3 <kamijooor@gmail.com>
2021-06-23 01:17:34 +0000

    add things for Japan time

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,8 +1,6 @@
 class Customers::CartItemsController < ApplicationController
 
-  helper_method :current_cart
-
-  def index
+def index
     @cart_items = CartItem.where(customer_id: current_customer.id)
     @total_price = 0
     @cart_items.each do |cart_item|
@@ -12,17 +10,16 @@ class Customers::CartItemsController < ApplicationController
 
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    @cart_item.save
     @cart_items = current_customer.cart_items.all
-    @cart_items.each do |cart_item|
-      if cart_item.product_id == @cart_item.product_id
-        new_amount = cart_item.amount + @cart_item.amount
-        cart_item.update_attribute(:amount, new_amount)
-        @cart_item.delete
-      end
-    end
-    @cart_item.save
-    redirect_to cart_items_path
+        @cart_items.each do |cart_item|
+          if cart_item.product_id == @cart_item.product_id
+            new_amount = cart_item.amount + @cart_item.amount
+            cart_item.update_attribute(:amount, new_amount)
+            @cart_item.delete
+          end
+        end
+        @cart_item.save
+        redirect_to cart_items_path
   end
 
   def update
