a9b9e99e5423cf19345b40bb4d9e1281a94da3b0
sakamototaisei <taisei0403089@gmail.com>
2021-06-23 00:50:19 +0000

    add hover

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,25 +56,3 @@ $(document).on('turbolinks:load', function () {
 
 
 // about文字
-$(document).on('turbolinks:load', function () {
-  $('#hoge').textillate({
-    loop: true,  
-    minDisplayTime: 3000,  
-    initialDelay: 2000,  
-    autoStart: true,  
-    in: {
-      effect: 'fadeIn',  
-      delayScale: 1.5,  
-      delay: 20,  
-      sync: false,  
-      shuffle: false
-      },  
-    out: {
-      effect: 'fadeOut',  
-      delayScale: 1.5,  
-      delay: 50,  
-      sync: false,  
-      shuffle: false
-      }
-  });
-})
